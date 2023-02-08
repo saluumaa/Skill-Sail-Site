b139a1aa65dc0a0dc45c2632e1214f94b986922b
saluumaa <salumahassan02@gmail.com>
2023-02-08 21:46:24 +0300

    changes to submodules

--- teachermobile.js
+++ teachermobile.js
@@ -1,31 +1,31 @@
 const speakers = [
   {
     name: 'Ilhan Muse',
-    image: '/assets/teacher-five-removebg-preview.png',
+    image: 'assets/teacher-five-removebg-preview.png',
     title: 'Lecturer at Amoud University',
     descripe: 'She has been teachin since 2016, her major is IT',
   },
   {
     name: 'Abdale Ali',
-    image: '/assets/teacher-two-removebg-preview.png',
+    image: 'assets/teacher-two-removebg-preview.png',
     title: 'Telesom Company',
     descripe: 'He is a head of the technical department at the Telesom Company',
   },
   {
     name: 'Edil Omer',
-    image: '/assets/teacher-two-removebg-preview (1).png',
+    image: 'assets/teacher-two-removebg-preview (1).png',
     title: 'Project manager and consultant',
     descripe: 'Edil works as a project manager and consultant in different companies',
   },
   {
     name: 'Salma Hassan',
-    image: '/assets/teacher-four-removebg-preview.png',
+    image: 'assets/teacher-four-removebg-preview.png',
     title: 'Software Developer',
     descripe: 'Salma is a software developer works with international companies ',
   },
   {
     name: 'Hibak Ahmed',
-    image: '/assets/teacher-one-removebg-preview.png',
+    image: 'assets/teacher-one-removebg-preview.png',
     title: 'Instructor at Amoud University',
     descripe: 'Hibak works as a finance and accounting specialist teacher',
   },
@@ -48,7 +48,7 @@ loadMoreBtn.addEventListener('click', () => {
     speakerDiv.className = 'teacher-wrapper';
     speakerDiv.innerHTML = `
       <div class="teacher-img">
-      <img src="/assets/speaker-back.png" alt="">
+      <img src="assets/speaker-back.png" alt="">
       <img src="${speaker.image}" alt="" class="overlay-one">
   </div>
   <div class="right-content">
@@ -78,7 +78,7 @@ for (let i = speakerCount; i < speakerCount + 5; i += 1) {
   teacherDiv.className = 'teacher-wrapper';
   teacherDiv.innerHTML = `
       <div class="teacher-img">
-      <img src="/assets/speaker-back.png" alt="">
+      <img src="assets/speaker-back.png" alt="">
       <img src="${speaker.image}" alt="" class="overlay-one">
   </div>
   <div class="right-content">
